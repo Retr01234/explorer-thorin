c9a1fd40a22c22f66b36ae1296f2b9b0fd01a445
Retr01234 <>
2023-08-02 14:06:37 +0000

    feature: add about page content

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
-from Flask import Flask, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -13,17 +13,17 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", page_title="About", list_of_numbers=[1, 2, 3])
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers")
 
 
 if __name__ == "__main__":
